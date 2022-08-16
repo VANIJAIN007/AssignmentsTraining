7305b10096b110c4b5255319bb2aa1a39fe031dc
Vani Jain <vani.jain_cs19@gla.ac.in>
2022-08-16 09:58:58 +0530

    update

--- Assignments/Function1.js
+++ Assignments/Function1.js
@@ -13,3 +13,29 @@ function myfunction1(){ //function defination
 
 }
 myfunction1();
+
+//function with arguments and without return
+
+function myfunction2(){
+    console.log("This is another function");
+}
+myfunction2();
+
+//2. Function with arguments and without return
+function myfunction1(a,b){
+    console.log("Function with data");
+    console.log("Result "+(a+b));
+}
+myfunction1(3,2);
+
+//3. Function with arguments and with return
+
+function myfunction3(a,b){
+    return a+b;
+}
+
+var result=myfunction3(10,2);
+console.log(result);//with console
+
+console.log("Data Received "+myfunction3(3,7));//without console
+
