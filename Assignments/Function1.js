7066093951cfeae18a39162a377a186f4d6bbab5
Vani Jain <vani.jain_cs19@gla.ac.in>
2022-08-16 09:51:58 +0530

    function concepts

---
+++ Assignments/Function1.js
@@ -0,0 +1,15 @@
+//function parts
+//1. Function Defination
+//2. Function Calling
+//3. Function Argumments(optional)
+//4. Return value->need to return data(optional)
+
+// Function Versions
+//1. Function without arguments and without return
+
+function myfunction1(){ //function defination
+    console.log("This is function");
+    console.log("It will perform some tasks");
+
+}
+myfunction1();
